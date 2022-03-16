75539a770efec52faca88f8ef1e6b50d2fcc2581
PaleRoom <PushistiiYashikWr@Yandex.ru>
2022-03-16 12:17:22 +0500

    Исправления в toString

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -17,8 +17,9 @@ public class Employee {
         this.country = country;
         this.age = age;
     }
- public String toString() {
-        return "Employee [id=" + id + ", firstName=" + firstName + ", " +
-     "lastName=" +  lastName + " country= " + country + ", " + " age=" + age + "]" ;
- }
+
+    public String toString() {
+        return "Employee [id=" + id + ", firstName=" + "'" + firstName + "'," +
+                " lastName=" + "'" + lastName + "'," + " country=" + "'" + country + "'," + " age=" + age + "]";
+    }
 }
